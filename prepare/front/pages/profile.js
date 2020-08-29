c3e2febe4d494e660f4e06bb3e58feaac74d6414
rktguswjd <010_cm@naver.com>
2020-08-29 19:50:10 +0900

    Next.js / 레이아웃 / 로그인 폼 만들기

---
+++ prepare/front/pages/profile.js
@@ -0,0 +1,18 @@
+import React from "react";
+import AppLayout from "../components/AppLayout";
+import Head from "next/head";
+
+const profile = () => {
+  return (
+    <>
+      <Head>
+        <title>내 프로필 | NodeBird</title>
+      </Head>
+      <AppLayout>
+        <div>내 프로필</div>
+      </AppLayout>
+    </>
+  );
+};
+
+export default profile;
