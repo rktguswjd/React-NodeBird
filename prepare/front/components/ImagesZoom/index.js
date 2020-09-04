a503fced849471b603a8bcce4fed6b0eb14c0413
rktguswjd <010_cm@naver.com>
2020-09-04 17:48:04 +0900

    글로벌 스타일과 컴포넌트 폴더구조 / 게시글 해시태그 링크로 만들기

--- prepare/front/components/ImagesZoom/index.js
+++ prepare/front/components/ImagesZoom/index.js
@@ -1,79 +1,24 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Slick from "react-slick";
-import styled from "styled-components";
-
-const Oberlay = styled.div`
-  position: fixed;
-  z-index: 5000;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-`;
-
-const Header = styled.header`
-  header: 44px;
-  background: white;
-  position: relative;
-  padding: 0;
-  text-align: center;
-
-  & h1 {
-    margin: 0;
-    font-size: 17px;
-    color: #333;
-    line-height: 44px;
-  }
-
-  & button {
-    position: absolute;
-    right: 0;
-    top: 0;
-    padding: 15px;
-    line-height: 14px;
-    cursor: pointer;
-  }
-`;
-
-const SlickWrapper = styled.div`
-  height: calc(100%-44px);
-  background: #090909;
-`;
-
-const ImgWrapper = styled.div`
-  padding: 32px;
-  text-align: center;
-
-  & img {
-    margin: 0 auto;
-    max-height: 750px;
-  }
-`;
-
-const Indicator = styled.div`
-  text-align: center;
-
-  & > div {
-    width: 75px;
-    height: 30px;
-    line-height: 30px;
-    border-radius: 15px;
-    background: #313131;
-    display: inline-block;
-    text-align: center;
-    color: white;
-    font-size: 15px;
-  }
-`;
+import {
+  Overlay,
+  Global,
+  Header,
+  CloseBtn,
+  SlickWrapper,
+  ImgWrapper,
+  Indicator,
+} from "./styles";
 
 const ImagesZoom = ({ images, onClose }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
   return (
-    <Oberlay>
+    <Overlay>
+      <Global />
       <Header>
         <h1>상세 이미지</h1>
-        <button onClick={onClose}>X</button>
+        <CloseBtn onClick={onClose}>X</CloseBtn>
       </Header>
       <SlickWrapper>
         <div>
@@ -91,9 +36,14 @@ const ImagesZoom = ({ images, onClose }) => {
               </ImgWrapper>
             ))}
           </Slick>
+          <Indicator>
+            <div>
+              {currentSlide + 1}/{images.length}
+            </div>
+          </Indicator>
         </div>
       </SlickWrapper>
-    </Oberlay>
+    </Overlay>
   );
 };
 
